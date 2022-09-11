63c53f50dcf96471bc39bfd8571094289ed715d5
Leonardo Firmo <leonardofirmodamota@gmail.com>
2022-09-11 16:19:56 -0300

    Recriando sistema

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-const negativo = document.querySelectorAll('.negativo')
-const positivo = document.querySelectorAll('.positivo')
-let valor = document.querySelectorAll('#valor')
+const produtos = document.querySelector('.produtos')
+const listaProdutos = Array.from(produtos.children)
 
 
 
@@ -8,92 +7,31 @@ let valor = document.querySelectorAll('#valor')
 
 
 
-
-
-// negativo
-
-negativo.forEach((item) => {
-    item.addEventListener('click',()=>{
-        let nomeItem = item.parentNode.parentNode.children[0].innerHTML
-        let quantidadeinner = item.parentNode.children[1].innerHTML
-        let precoItem = item.parentNode.parentNode.children[1].innerHTML
-        let quantidade = item.parentNode.children[1]
-      
-       
-
-        if(quantidade.innerHTML > 0){
-            let quantidade = item.parentNode.children[1]
-            quantidade.innerHTML= Number(quantidade.innerHTML)-1;
-
-
-            valor[0].innerHTML = `<h2>Carrinho Atual </h2><br>
-            Salgado:${nomeItem} <br>
-            Quantidade:${quantidade.innerHTML}<br>
-            Preço:${ (Number(precoItem) * Number(quantidade.innerHTML)).toFixed(2).replace(".",",")}` 
-            
-            
-
-
-
-
-               
-            if(quantidade.innerHTML ==0){
-                quantidade.style.color='black'
-            }
-        
-        }
-      
-    })
+function verificaQuantidade(){
+  
+    listaProdutos.forEach(item=>{
+        const negativo = item.children[1].children[2].children[0]
+        let quantidade = item.children[1].children[2].children[1]
+        const positivo = item.children[1].children[2].children[2]
     
-    
-});
-
-// positivo
-positivo.forEach((item) => {
-    item.addEventListener('click',()=>{
-        let ParagraphQuantidade = item.parentNode.children[1]
-        let quantidade = item.parentNode.children[1].innerHTML
-        let nomeItem = item.parentNode.parentNode.children[0].innerHTML
-        let precoItem = item.parentNode.parentNode.children[1].innerHTML
-     
         
-        if(quantidade >= 0){
-            ParagraphQuantidade.style.color='green'
-            ParagraphQuantidade.innerHTML= Number(quantidade)+1
-
-          
-            // let pedido= {
-            //     nome:nomeItem,
-            //     preco:precoItem,
-            //     qtd:Number(quantidade)
-            // }
-        
-
-            let pedido= [Number(quantidade) +1,nomeItem,precoItem]
-            let pedidoAtual = []
-
-            console.log(pedidoAtual[1],pedido[1])
-            pedidoAtual+= pedido
-            
-
-            
+        negativo.addEventListener('click',()=>{
            
-     
-     
-          
-            
-
-
-        }
-
-        
-       
-        
-       
-       
+            // console.log(negativo)
+            quantidade.innerHTML --
+           
+    
+        })
+      
+        positivo.addEventListener('click',()=>{
+           quantidade.innerHTML++
+            // console.log(positivo)
+    
+        })
+    
+    
     })
     
-});
+}
 
-
-
+verificaQuantidade()
