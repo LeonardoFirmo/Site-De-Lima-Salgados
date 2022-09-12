eee8768e5ad0bd165e8bd6ba9a9f88cb1dac3e61
Leonardo Firmo <leonardofirmodamota@gmail.com>
2022-09-12 15:55:08 -0300

    Sistema Funcional

--- script.js
+++ script.js
@@ -1,15 +1,62 @@
 const produtos = document.querySelector('.produtos')
 const listaProdutos = Array.from(produtos.children)
+const clienteResumoPedido = document.querySelector('#valor')
+const calculaPedido = document.querySelector('#calcularPedido')
+const enviarPedido = document.querySelector('#enviarPedido')
 
 
 
 
+let pedidoFinal = [
+ { nomeProduto:'Coxinha de frango',precoProduto:1.60,somaPreco:0,quantidadeProduto:0},
+ { nomeProduto:'Esfirra',precoProduto:2.60,somaPreco:0,quantidadeProduto:0},
+ { nomeProduto:'Coxinha de calabresa',precoProduto:1.90,somaPreco:0,quantidadeProduto:0},
+
+]
+
+
+function somaPrecos(){
+    let soma = 0
+    pedidoFinal.map(item =>{
+        
+        soma += Number(item.somaPreco)
+        
+        
+    })
+    return soma.toFixed(2)
+}
+
+
+function mostraPedidoCliente(){
+    clienteResumoPedido.innerHTML=''
+    let precoTotal= somaPrecos()
+
+    pedidoFinal.forEach(item =>{
+       if(item.quantidadeProduto > 0){
+        clienteResumoPedido.innerHTML+= `${item.nomeProduto}<br>
+        Preço Somado:${item.somaPreco}<br>
+        Quantidade:${item.quantidadeProduto}<br>
+        --------------------------------<br>`} 
+
+     
+    })
+   
+    if(precoTotal > 0){
+        clienteResumoPedido.innerHTML+= `<h2>Valor Total:${precoTotal}</h2>`
+    }
+    enviarPedido.style.display='flex'
+    
+}
+
+calculaPedido.addEventListener('click',mostraPedidoCliente)
 
 
 
 function verificaQuantidade(){
+   
+        
   
-    listaProdutos.forEach(item=>{
+    listaProdutos.forEach((item)=>{
         const negativo = item.children[1].children[2].children[0]
         let quantidade = item.children[1].children[2].children[1]
         const positivo = item.children[1].children[2].children[2]
@@ -17,17 +64,43 @@ function verificaQuantidade(){
         
         negativo.addEventListener('click',()=>{
            
-            // console.log(negativo)
+          
             quantidade.innerHTML --
-           
+          
+            pedidoFinal.forEach( (itemPF) => {
+               
+                if(itemPF.nomeProduto === item.children[1].children[0].innerText){
+                   itemPF.quantidadeProduto --
+
+                   itemPF.somaPreco =  (itemPF.precoProduto * itemPF.quantidadeProduto).toFixed(2)
+                }
+
+            });
     
         })
       
         positivo.addEventListener('click',()=>{
-           quantidade.innerHTML++
-            // console.log(positivo)
-    
+        
+            quantidade.innerHTML++
+            
+          
+            pedidoFinal.forEach( (itemPF) => {
+               
+                if(itemPF.nomeProduto === item.children[1].children[0].innerText){
+                   itemPF.quantidadeProduto ++
+                    
+                   itemPF.somaPreco =  (itemPF.precoProduto * itemPF.quantidadeProduto).toFixed(2)
+                  
+                   
+                }
+
+            });
+           
+           
+   
+           
         })
+       
     
     
     })
@@ -35,3 +108,4 @@ function verificaQuantidade(){
 }
 
 verificaQuantidade()
+
