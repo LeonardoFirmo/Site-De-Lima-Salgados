099523d70fdc12d9052eb82c7b1583183a0fd44d
Leonardo Firmo <leonardofirmodamota@gmail.com>
2022-11-18 14:00:11 -0300

    atualização erros

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const produtos = document.querySelector('.produtos')
-const salgadosFritos = document.querySelector('.salgados-fritos')
 const clienteResumoPedido = document.querySelector('#valor')
 const novoPedidoH3 = document.querySelector('#novoPedido')
 const calculaPedido = document.querySelector('#calcularPedido')
@@ -11,27 +10,6 @@ enviarPedido.addEventListener('click', enviarPedidoZap)
 
 
 let pedidoFinal = [
-<<<<<<< HEAD
- { nome:'Coxinha de frango',tipo:'Salgados Fritos',preco:1.80,somaPreco:0,quantidade:0,img:'coxinhaDeFrango.jpg'},
- { nome:'Coxinha de frango c/ catupiry',tipo:'Salgados Fritos',preco:2.00,somaPreco:0,quantidade:0,img:'coxinhaDeFrango.jpg'},
- { nome:'Bolinho de queijo',tipo:'Salgados Fritos',preco:1.80,somaPreco:0,quantidade:0,img:'esfirra.jpg'},
- { nome:'Bolinho de carne',tipo:'Salgados Fritos',preco:1.80,somaPreco:0,quantidade:0,img:'coxinhaDeCalabresa.jpg'},
- { nome:'Bolinho de calabresa',tipo:'Salgados Fritos',preco:1.80,somaPreco:0,quantidade:0,img:'pastelDeCarne.jpg'},
- { nome:'Risole',tipo:'Salgados Fritos',preco:1.80,somaPreco:0,quantidade:0,img:'empadao.jpg'},
- { nome:'Kibe',tipo:'Salgados Fritos',preco:1.80,somaPreco:0,quantidade:0,img:'empadao.jpg'},
-
- { nome:'Empada de frango',tipo:'Empadas',preco:3.00,somaPreco:0,quantidade:0,img:'empadao.jpg'},
- { nome:'Empada de frango c/ catupiry',tipo:'Empadas',preco:3.50,somaPreco:0,quantidade:0,img:'empadao.jpg'},
-
- { nome:'Esfiha de carne',tipo:'Esfiha',preco:3.00,somaPreco:0,quantidade:0,img:'empadao.jpg'},
- { nome:'Esfiha de queijo',tipo:'Esfiha',preco:3.00,somaPreco:0,quantidade:0,img:'empadao.jpg'},
- { nome:'Esfiha de frango',tipo:'Esfiha',preco:3.00,somaPreco:0,quantidade:0,img:'empadao.jpg'},
- { nome:'Esfiha de frango c/ catupiry',tipo:'Esfiha',preco:3.50,somaPreco:0,quantidade:0,img:'empadao.jpg'},
- { nome:'Esfiha de Calabresa',tipo:'Esfiha',preco:3.00,somaPreco:0,quantidade:0,img:'empadao.jpg'},
- { nome:'Esfiha de catupiry',tipo:'Esfiha',preco:3.00,somaPreco:0,quantidade:0,img:'empadao.jpg'},
- { nome:'Esfiha de catupiry com milho',tipo:'Esfiha',preco:3.50,somaPreco:0,quantidade:0,img:'empadao.jpg'},
-
-=======
     { nome:'Coxinha de frango',tipo:'Salgados Fritos',preco:1.80,somaPreco:0,quantidade:0,img:'coxinhaDeFrango.jpg'},
     { nome:'Coxinha de frango c/ catupiry',tipo:'Salgados Fritos',preco:2.00,somaPreco:0,quantidade:0,img:'coxinhaDeFrango.jpg'},
     { nome:'Bolinho de queijo',tipo:'Salgados Fritos',preco:1.80,somaPreco:0,quantidade:0,img:'esfirra.jpg'},
@@ -50,41 +28,19 @@ let pedidoFinal = [
     { nome:'Esfiha de Calabresa',tipo:'Esfiha',preco:3.00,somaPreco:0,quantidade:0,img:'empadao.jpg'},
     { nome:'Esfiha de catupiry',tipo:'Esfiha',preco:3.00,somaPreco:0,quantidade:0,img:'empadao.jpg'},
     { nome:'Esfiha de catupiry com milho',tipo:'Esfiha',preco:3.50,somaPreco:0,quantidade:0,img:'empadao.jpg'},
->>>>>>> b6
 ]
 
-// 
+renderizaProdutos(pedidoFinal)
 
+function renderizaProdutos(product){   
+    product.forEach(item =>{
 
-function renderizaProdutos(product,itemClicado){
-   
+        produtos.innerHTML+=`
+        <div class="produtoWrapper">
+            <img class="imgProdutos" src="img/coxinha.jpg" alt="">
 
-    product.filter(item =>{
-        
-        console.log(itemClicado);
-        // console.log(item.tipo, itemClicado);
-        // console.log(item.tipo === itemClicado);
-        if(itemClicado === 'Salgados Fritos' && item.tipo === 'Salgados Fritos' ){
+            <div class="produto">
 
-<<<<<<< HEAD
-           
-            
-            produtos.children[0].innerHTML+=`
-            <div class="produtoWrapper">
-                <img class="imgProdutos" src="img/coxinha.jpg" alt="">
-    
-                <div class="produto">
-    
-                    <p id="precoDescricao">${item.nome}</p>
-                    <p id="">${item.preco.toFixed(2).replace('.',',')}</p> 
-    
-                    <div class="botoes" style="display:flex">
-                        <button class="bt negativo">-</button> 
-                        <p class="quantidade">0</p> 
-                        <button class="bt positivo">+</button>
-                    </div>
-    
-=======
                 <p id="precoDescricao">${item.nome}</p>
                 <p id="precoDescricao">${item.preco.toFixed(2).replace('.',',')}</p> 
 
@@ -92,166 +48,17 @@ function renderizaProdutos(product,itemClicado){
                     <button class="bt negativo">-</button> 
                     <p class="quantidade">0</p> 
                     <button class="bt positivo">+</button>
->>>>>>> b6
                 </div>
-            </div>`
 
-        
-
-        }else if(itemClicado === 'Esfiha' && item.tipo === 'Esfiha'){
-
-            produtos.children[1].innerHTML+=`
-            <div class="produtoWrapper">
-                <img class="imgProdutos" src="img/coxinha.jpg" alt="">
-    
-                <div class="produto">
-    
-                    <p id="precoDescricao">${item.nome}</p>
-                    <p id="">${item.preco.toFixed(2).replace('.',',')}</p> 
-    
-                    <div class="botoes" style="display:flex">
-                        <button class="bt negativo">-</button> 
-                        <p class="quantidade">0</p> 
-                        <button class="bt positivo">+</button>
-                    </div>
-    
-                </div>
-            </div>`
-            
-
-        }else if(itemClicado === 'Empadas' && item.tipo === 'Empadas'){
-            
-            produtos.children[2].innerHTML+=`
-            <div class="produtoWrapper">
-                <img class="imgProdutos" src="img/coxinha.jpg" alt="">
-    
-                <div class="produto">
-    
-                    <p id="precoDescricao">${item.nome}</p>
-                    <p id="">${item.preco.toFixed(2).replace('.',',')}</p> 
-    
-                    <div class="botoes" style="display:flex">
-                        <button class="bt negativo">-</button> 
-                        <p class="quantidade">0</p> 
-                        <button class="bt positivo">+</button>
-                    </div>
-    
-                </div>
-            </div>`
-
-        }
-        
-       
+            </div>
+        </div>`
         
     })
-    
+
     const listaProdutos = Array.from(produtos.children)
-    
-    // verificaQuantidade(listaProdutos)
+    verificaQuantidade(listaProdutos)
 
 }
-
-
-
-
-
-
-produtos.addEventListener('click',RenderizaTipoDeProduto)
-
-function RenderizaTipoDeProduto (event){
-    const itemClicado = event.target.innerHTML
-    const tamanhoLista =event.target.parentNode.children.length
-    const verificaItemClicado=  itemClicado === 'Salgados Fritos' || itemClicado === 'Empadas' || itemClicado === 'Esfiha'
-    
-    
-    
-    if(tamanhoLista > 1  && verificaItemClicado){
-       
-        // event.target.parentNode.style.display='none'
-       event.target.parentNode.innerHTML=`<h2>${itemClicado}</h2>`
-    }else{
-        renderizaProdutos(pedidoFinal,itemClicado)
-    }
-    
-   
-
-
-}
-
-
-function verificaQuantidade(listaProdutos){
-
-
-
-    listaProdutos[0].forEach((item)=>{
-
-        
-        const negativo = item.children[1].children[2].children[0]
-        let quantidade = item.children[1].children[2].children[1]
-        const positivo = item.children[1].children[2].children[2]
-        
-    
-        
-        negativo.addEventListener('click',()=>{
-           
-  
-            if(quantidade.innerHTML-1 <= 0){
-                quantidade.style.color=''
-                quantidade.innerHTML=0
-                
-              
-            }else{
-                quantidade.innerHTML --
-                
-            }
-          
-            pedidoFinal.forEach( (itemPF) => {
-
-                if(itemPF.quantidade <= 0){
-                    itemPF.quantidade = 0
-
-                }else if(itemPF.nome === item.children[1].children[0].innerText){
-                   itemPF.quantidade --
-                   itemPF.somaPreco =  (itemPF.preco * itemPF.quantidade).toFixed(2)
-                   mostraPedidoCliente()
-                }
-
-            });
-    
-        })
-      
-        positivo.addEventListener('click',()=>{
-        
-            quantidade.innerHTML++
-           
-            if(quantidade.innerHTML >=1){
-                quantidade.style.color='green'
-            }else{
-                quantidade.style.color=''
-            }
-          
-            pedidoFinal.forEach( (itemPF) => {
-               
-                if(itemPF.nome === item.children[1].children[0].innerText){
-                   itemPF.quantidade ++
-                   itemPF.somaPreco =  (itemPF.preco * itemPF.quantidade).toFixed(2)
-                   mostraPedidoCliente()
-                   
-                }
-
-            });
-           
-
-        })
-
-    
-    })
-    
-}
-
-
-
-// Finaliza pedido  SOMA / MOSTRA / ENVIA PEDIDO
 
 
 function somaPrecos(){
@@ -294,9 +101,6 @@ function enviarPedidoZap(){
     let texto = `✅ NOVO PEDIDO\n------------------------------\n▶ RESUMO DO PEDIDO\n------------------------------\n${clienteResumoPedido.innerText}` ;
     let pedido = window.encodeURIComponent(texto);
     window.open("https://api.whatsapp.com/send?phone=5521980511433&text="+pedido)
-<<<<<<< HEAD
-}
-=======
 }
 
 
@@ -370,4 +174,3 @@ function verificaQuantidade(listaProdutos){
 
 
 
->>>>>>> b6
